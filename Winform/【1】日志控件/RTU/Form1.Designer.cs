aeabdabb062da35821019e2cf099c6b031f467d3
Wesley <72319983+lzz-world@users.noreply.github.com>
2023-11-27 19:30:06 +0800

    Add files via upload

---
+++ Winform/【1】日志控件/RTU/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace RTU
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReadRegistersButton = new System.Windows.Forms.Button();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.StartAddressTextBox = new System.Windows.Forms.TextBox();
+            this.RegistersListBox = new System.Windows.Forms.ListBox();
+            this.NumRegistersTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ReadRegistersButton
+            // 
+            this.ReadRegistersButton.Location = new System.Drawing.Point(594, 150);
+            this.ReadRegistersButton.Name = "ReadRegistersButton";
+            this.ReadRegistersButton.Size = new System.Drawing.Size(75, 23);
+            this.ReadRegistersButton.TabIndex = 0;
+            this.ReadRegistersButton.Text = "读取";
+            this.ReadRegistersButton.UseVisualStyleBackColor = true;
+            this.ReadRegistersButton.Click += new System.EventHandler(this.ReadRegisters_Click);
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Location = new System.Drawing.Point(529, 63);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(75, 23);
+            this.BtnClose.TabIndex = 1;
+            this.BtnClose.Text = "关闭";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // StartAddressTextBox
+            // 
+            this.StartAddressTextBox.Location = new System.Drawing.Point(414, 101);
+            this.StartAddressTextBox.Name = "StartAddressTextBox";
+            this.StartAddressTextBox.Size = new System.Drawing.Size(100, 21);
+            this.StartAddressTextBox.TabIndex = 2;
+            this.StartAddressTextBox.Text = "起始地址";
+            this.StartAddressTextBox.TextChanged += new System.EventHandler(this.StartAddressTextBox_TextChanged);
+            // 
+            // RegistersListBox
+            // 
+            this.RegistersListBox.FormattingEnabled = true;
+            this.RegistersListBox.ItemHeight = 12;
+            this.RegistersListBox.Items.AddRange(new object[] {
+            "寄存器列表"});
+            this.RegistersListBox.Location = new System.Drawing.Point(456, 150);
+            this.RegistersListBox.Name = "RegistersListBox";
+            this.RegistersListBox.Size = new System.Drawing.Size(120, 88);
+            this.RegistersListBox.TabIndex = 3;
+            // 
+            // NumRegistersTextBox
+            // 
+            this.NumRegistersTextBox.Location = new System.Drawing.Point(569, 101);
+            this.NumRegistersTextBox.Name = "NumRegistersTextBox";
+            this.NumRegistersTextBox.Size = new System.Drawing.Size(100, 21);
+            this.NumRegistersTextBox.TabIndex = 4;
+            this.NumRegistersTextBox.Text = "寄存器数量";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NumRegistersTextBox);
+            this.Controls.Add(this.RegistersListBox);
+            this.Controls.Add(this.StartAddressTextBox);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.ReadRegistersButton);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ReadRegistersButton;
+        private System.Windows.Forms.Button BtnClose;
+        private System.Windows.Forms.TextBox StartAddressTextBox;
+        private System.Windows.Forms.ListBox RegistersListBox;
+        private System.Windows.Forms.TextBox NumRegistersTextBox;
+    }
+}
+
---
+++ Winform/【1】日志控件/RTU/LogsControl.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Threading;
+
+namespace RTU
+{
+    public partial class LogsControl : UserControl
+    {
+        private List<BaseLog> _allLogs = new List<BaseLog>();
+        private static LogLevel _logMarkLevel = LogLevel.All;
+        private int _infoCount = 0, _warnCount = 0, _errorCount = 0;
+
+        public LogsControl()
+        {
+            InitializeComponent();
+
+            this.LogListView.Columns.Add("日志", this.Width);
+            this.LogListView.HeaderStyle = ColumnHeaderStyle.None;
+
+            #region 测试添加
+#if DEBUG
+            this.AddBtn.Visible = true;
+            Random random = new Random();
+            this.AddBtn.Click += (s, e) =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    switch (random.Next(0, 3))
+                    {
+                        case 0: LogInfo("-------------------------Info"); break;
+                        case 1: LogWarn("-------------------------Warn"); break;
+                        case 2: LogError("-------------------------Error"); break;
+                        default:
+                            break;
+                    }
+                };
+            };
+
+            bool autoAddLog = false;
+            this.AutoAddBtn.Click += (s, e) =>
+            {
+                autoAddLog = !autoAddLog;
+                if (AutoAddBtn.Checked == true)
+                {
+                    Task.Run(async () =>
+                    {
+                        while (autoAddLog)
+                        {
+                            for (int i = 0; i < 100; i++)
+                            {
+                                switch (random.Next(0, 3))
+                                {
+                                    case 0: LogInfo("-------------------------Info"); break;
+                                    case 1: LogWarn("-------------------------Warn"); break;
+                                    case 2: LogError("-------------------------Error"); break;
+                                    default:
+                                        break;
+                                }
+                            };
+                            await Task.Delay(1000);
+                        }
+                    });
+                }
+            };
+#else
+            this.AddBtn.Visible = false;
+            this.AutoAddBtn.Visible = false;
+#endif
+            #endregion
+        }
+
+        private void InvokeLog(LogLevel level, string message, Color color)
+        {
+            // 检查是否需要 Invoke 到 UI 线程
+            if (LogListView.InvokeRequired)
+            {
+                // 如果当前线程不是 UI 线程，则使用 Invoke 将操作传递到 UI 线程
+                LogListView.Invoke((MethodInvoker)delegate
+                {
+                    // 在 UI 线程上执行更新操作
+                    // 例如：更新 LogListView 控件的操作
+                    Log(level, message, color);
+                });
+            }
+            else
+            {
+                // 在 UI 线程上执行更新操作
+                // 例如：更新 LogListView 控件的操作
+                Log(level, message, color);
+            }
+        }
+
+        private void Log(LogLevel level, string message, Color color)
+        {
+            DateTime currentTime = DateTime.Now;
+            string milliseconds = currentTime.Millisecond.ToString("000");
+            var log = new BaseLog { Level = level, Message = message, LogColor = color, DateTime = $"{currentTime}:{milliseconds}" };
+            _allLogs.Add(log);
+
+            this.LogListView.BeginUpdate();
+
+            // 根据选择等级添加显示
+            if (log.Level == _logMarkLevel)
+            {
+                this.LogListView.Items.Add(new ListViewItem()
+                {
+                    Text = $"{log.DateTime}     {log.Message}",
+                    ForeColor = log.LogColor
+                });
+            }
+            else if (_logMarkLevel == LogLevel.All)
+            {
+                this.LogListView.Items.Add(new ListViewItem()
+                {
+                    Text = $"{log.DateTime}     {log.Message}",
+                    ForeColor = log.LogColor
+                });
+            }
+
+            switch (level)
+            {
+                case LogLevel.Info: _infoCount += 1; InfoBtn.Text = $"正常({_infoCount})"; break;
+                case LogLevel.Warn: _warnCount += 1; WarnBtn.Text = $"警告({_warnCount})"; break;
+                case LogLevel.Error: _errorCount += 1; ErrorBtn.Text = $"报警({_errorCount})"; break;
+                default: break;
+            }
+
+            // 自动滚动
+            if (AutoRollBtn.Checked)
+            {
+                if (LogListView.Items.Count > 0)
+                    LogListView.EnsureVisible(LogListView.Items.Count - 1);
+            }
+
+            // 日志限制数量
+            if (_allLogs.Count > 3000)
+            {
+                var indexLevel = _allLogs.First();
+
+                if (LogListView.Items.Count > 0)
+                {
+                    var viewItem = LogListView.Items[0];
+                    if (viewItem.Text == $"{indexLevel.DateTime}     {indexLevel.Message}" &&
+                        viewItem.ForeColor == indexLevel.LogColor)
+                        LogListView.Items.RemoveAt(0);
+                }
+                _allLogs.RemoveAt(0);
+
+                switch (indexLevel.Level)
+                {
+                    case LogLevel.Info: _infoCount -= 1; InfoBtn.Text = $"正常({_infoCount})"; break;
+                    case LogLevel.Warn: _warnCount -= 1; WarnBtn.Text = $"警告({_warnCount})"; break;
+                    case LogLevel.Error: _errorCount -= 1; ErrorBtn.Text = $"报警({_errorCount})"; break;
+                    default: break;
+                }
+            }
+
+            this.LogListView.EndUpdate();
+        }
+
+        private void FilterLog(IEnumerable<BaseLog> logs)
+        {
+            this.LogListView.BeginUpdate();
+            this.LogListView.Items.Clear();
+
+            List<ListViewItem> subViewItems = new List<ListViewItem>();
+
+            foreach (var log in logs)
+                subViewItems.Add(new ListViewItem()
+                {
+                    Text = $"{log.DateTime}     {log.Message}",
+                    ForeColor = log.LogColor
+                });
+
+            this.LogListView.Items.AddRange(subViewItems.ToArray());
+            this.LogListView.EndUpdate();
+
+            // 自动滚动
+            if (AutoRollBtn.Checked)
+            {
+                if (LogListView.Items.Count > 0)
+                    LogListView.EnsureVisible(LogListView.Items.Count - 1);
+            }
+        }
+
+        /// <summary>
+        /// 打印正常信息
+        /// </summary>
+        /// <param name="text"></param>
+        public void LogInfo(string text) => InvokeLog(LogLevel.Info, text, Color.Black);
+
+        /// <summary>
+        /// 记录警告信息
+        /// </summary>
+        /// <param name="text"></param>
+        public void LogWarn(string text) => InvokeLog(LogLevel.Warn, text, Color.Blue);
+
+        /// <summary>
+        /// 记录错误信息
+        /// </summary>
+        /// <param name="text"></param>
+        public void LogError(string text) => InvokeLog(LogLevel.Error, text, Color.Red);
+
+        private void AllBtn_Click(object sender, EventArgs e)
+        {
+            _logMarkLevel = LogLevel.All;
+            FilterLog(_allLogs.Select(s => s).ToList());
+        }
+        private void InfoBtn_Click(object sender, EventArgs e)
+        {
+            _logMarkLevel = LogLevel.Info;
+            FilterLog(_allLogs.Where(w => w.Level == LogLevel.Info).ToList());
+        }
+        private void WarnBtn_Click(object sender, EventArgs e)
+        {
+            _logMarkLevel = LogLevel.Warn;
+            FilterLog(_allLogs.Where(w => w.Level == LogLevel.Warn).ToList());
+        }
+        private void ErrorBtn_Click(object sender, EventArgs e)
+        {
+            _logMarkLevel = LogLevel.Error;
+            FilterLog(_allLogs.Where(w => w.Level == LogLevel.Error).ToList());
+        }
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            this.LogListView.Items.Clear();
+            _allLogs.Clear();
+            _infoCount = 0;
+            _warnCount = 0;
+            _errorCount = 0;
+
+            InfoBtn.Text = $"正常({_infoCount})";
+            WarnBtn.Text = $"警告({_warnCount})";
+            ErrorBtn.Text = $"报警({_errorCount})";
+        }
+    }
+
+    enum LogLevel
+    {
+        All,
+        Info,
+        Warn,
+        Error
+    }
+
+    class BaseLog
+    {
+        public LogLevel Level { get; set; }
+
+        public string DateTime { get; set; }
+
+        public string Message { get; set; }
+
+        public Color LogColor { get; set; }
+    }
+}
